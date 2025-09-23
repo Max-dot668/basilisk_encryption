ba679a3a7bbdbbc59d96f664bef4e2a5cb137a49
Max-dot668 <maxiyouit@gmail.com>
2025-09-23 14:25:17 -0700

    Add initial project structure layout

---
+++ main.py
@@ -0,0 +1,29 @@
+# Parseltongue mapping dictionary
+BASILISK_DICT = {
+    'a': 'haa', 'b': 'ethaa', 'c': 'hass', 'd': 'ssaah',
+    'e': 'ssss', 'f': 'hassay', 'g': 'ehhh', 'h': 'seethaaa',
+    'i': 'aayaa', 'j': 'sheaaya', 'k': 'ayaahath', 'l': 'hathehh',
+    'm': 'ssssehhh', 'n': 'seyythaa', 'o': 'ayaeeh', 'p': 'aaah',
+    'q': 'atheya', 'r': 'hasseey', 's': 'ssssaytha', 't': 'ssseya',
+    'u': 'esseytha', 'v': 'saahthay', 'w': 'hatheeey', 'x': 'seeeeeh',
+    'y': 'haaaaaah', 'z': 'ssssseettth'
+}
+
+def encrypt(text):
+    ...
+
+def decrypt(text):
+    ...
+
+def encrypt_file(input_file, output_file):
+    ...
+
+def decrypt_file(input_file, output_file):
+    ...
+
+def main():
+    print('Basilisk Encryption 🐍')
+
+
+if __name__ == "__main__":
+    main()
