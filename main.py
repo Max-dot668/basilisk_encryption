14cc711cbec3fc13f0ad276ef3fd49d24e0b8feb
Max-dot668 <maxiyouit@gmail.com>
2025-09-23 20:25:34 -0700

    Refactor encrpytion program

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-# Parseltongue mapping dictionary
-SECRET_KEY = {
-    'a': 'haa', 'b': 'ethaa', 'c': 'hass', 'd': 'ssaah',
-    'e': 'ssss', 'f': 'hassay', 'g': 'ehhh', 'h': 'seethaaa',
-    'i': 'aayaa', 'j': 'sheaaya', 'k': 'ayaahath', 'l': 'hathehh',
-    'm': 'ssssehhh', 'n': 'seyythaa', 'o': 'ayaeeh', 'p': 'aaah',
-    'q': 'atheya', 'r': 'hasseey', 's': 'ssssaytha', 't': 'ssseya',
-    'u': 'esseytha', 'v': 'saahthay', 'w': 'hatheeey', 'x': 'seeeeeh',
-    'y': 'haaaaaah', 'z': 'ssssseettth'
-}
+import re
+
+# Binary mapping symbols 
+SYMBOLS = { '00': ♠, '01': ♥, '10': ♦, '11': ♣ }
+
+# Each letter in the alphabet mapped to a number index
+letter_to_number = { 'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7,
+                    'I': 8, 'J': 9, 'K': 10, 'L': 11, 'M': 12, 'N': 13, 'O': 14, 'P': 15,
+                    'Q': 16, 'R': 17, 'S': 18, 'T': 19, 'U': 20, 'V': 21, 'W': 22,'X': 23,
+                    'Y': 24, 'Z': 25 }
+
+
+# ================================================================================
+# Function implementations
+# ================================================================================
 
 def encrypt(text, key):
     ...
 
-def decrypt(text, key):
+def decrypt(ciphertext, key):
     ...
 
 def encrypt_file(input_file, output_file, key):
@@ -21,8 +26,50 @@ def encrypt_file(input_file, output_file, key):
 def decrypt_file(input_file, output_file, key):
     ...
 
+# ================================================================================
+# Helper functions
+# ================================================================================
+
+def validate_input(key):
+    """
+    Raises ValueError if key contains digits.
+    Only letters and special characters are allowed 
+    """
+    pattern = r'\d' # matches digits
+    if match := re.search(pattern, key):
+        return False
+    return True
+
+def binary_conversion(n):
+    """
+    Returns a string binary represation of a decimal
+    number through the eucledian algorithm
+    """
+    result = ''
+    while n != 0:
+        result += str(n % 2)
+        n = n // 2
+    return result[::-1]
+
+# ================================================================================
+# Main program 
+# ================================================================================
 def main():
-    print('Basilisk Encryption 🐍')
+    print('Suite Encryption Script')
+    print('(1) Encrypt a file')
+    print('(2) Decrypt a file')
+
+    # user choice with input validation
+    while True:
+        try:
+            choice = int(input('Enter choice here: '))
+            if choice in [1, 2]:
+                break
+            else:
+                print('Invalid choice. Please enter 1 or 2')
+        except ValueError:
+            print('Invalid input. Please enter 1 or 2')
+
 
 
 if __name__ == "__main__":
